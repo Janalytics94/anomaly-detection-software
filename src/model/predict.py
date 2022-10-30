8de3397c5ffc76a8d8026c2b70345926531b8946
Janalytics944 <jvihs@yahoo.com>
2022-10-30 14:25:44 +0100

    aufgeräumt

--- src/model/predict.py
+++ src/model/predict.py
@@ -8,35 +8,23 @@ from sklearn.neighbors import LocalOutlierFactor
 
 
 class A_Model():
-    def __init__(self, path, model_type):
-        self.path = path
-        self.model_type = model_type # -> think of possible model_types 
-        data = __read_data__(self.path) 
-
-        def __read_data__(path):
-            
-            # read in the data 
-            data = pd.read_pickle(path)
-            
-            return data
-        
     
-    # read in yaml
-    params = dvc.api.params_show('src/model/params.yml')
-
-    def load_model(model_type, params):
-
-        if model_type == 'IsolationForest': 
-            hyper_parameter = params['IsolationForest'] #dictionary of hyper_params 
+    def __init__(self, model_type):
+        self.model_type = model_type # -> think of possible model_types 
+        self.hyper_params = dvc.api.params_show('/Users/janavihs/projects/anomaly-detection-software/src/model/params.yml')
+        
+    def load_model(self):
+        if self.model_type == 'IsolationForest': 
+            hyper_parameter = self.hyper_params['IsolationForest'] #dictionary of hyper_params 
             model = IsolationForest(hyper_parameter)
         
-        if model_type == 'LocalOutlierFactor':
-            hyper_parameter = params['LocalOutlierFactor']
-            model = LocalOutlierFactor
+        if self.model_type == 'LocalOutlierFactor':
+            hyper_parameter = self.hyper_params['LocalOutlierFactor']
+            model = LocalOutlierFactor(hyper_parameter)
         
         return model
 
-    def fit_(model, data):
+    def fit_(self, model, data):
         """
         Returns fitted model 
         
@@ -44,7 +32,7 @@ class A_Model():
         # Gefittet des model noch abspeichern für Fraunhofer?
         return model.fit(data)
 
-    def predict_(model, data):
+    def predict_(self, model, data):
 
         """
         Predicts if data point is a anomaly, calculates score function and adds it to the dataframe.
@@ -61,7 +49,7 @@ class A_Model():
         
         return predictions, score, data
 
-    def evaluate():
+    def evaluate(self):
 
 
         return
@@ -72,4 +60,4 @@ class A_Model():
 
         # calcualte f1 score
         # confusion matrix 
-       
+    
