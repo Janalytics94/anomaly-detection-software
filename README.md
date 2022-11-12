a137fbc5f292ea79754811a67db73efabb609e4c
Janalytics944 <jvihs@yahoo.com>
2022-11-12 15:13:08 +0100

    LOF succesfully implemented, added scaling

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 ### The Data Scenarios
 https://github.com/LID-DS/LID-DS
+
+https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0152173#sec008    
+
+https://github.com/Deffro/Data-Science-Portfolio/blob/master/Notebooks/Outlier%20Detection/Outlier%20Detection%20-%20Theory%2C%20Visualizations%20and%20Code.ipynb
+
+
+https://www.infoq.com/articles/system-behaviour-time-series-ml/
--- src/model/helpers.py
+++ src/model/helpers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import pandas as pd
+from sklearn import preprocessing
 
 # Functions 
 def calc_time_delta(df:pd.DataFrame) -> pd.DataFrame:
@@ -135,6 +136,43 @@ def outlier_truncate(df:pd.DataFrame) -> pd.DataFrame:
     no_outliers =df[~((df<(Q1-1.5*IQR)) | (df>(Q3+1.5*IQR)))]
     
     return no_outliers
+
+
+def scale(df: pd.DataFrame, method: str):
+    """
+    Params:
+        - methods : standard, min_max, max_abs, power
+        - x : columns that should be scaled in df.
+
+    Returns:
+        - x_scaled : scaled value according to chosen method
+
+    """
+
+    columns = df.columns.tolist()
+    columns = [column for column in columns if column not in ("dates", "times", "container_name", "exploit", "timestamp_container_ready" , "timestamp_trick_admin" , "timestamp_execute_reverse_shell" , "timestamp_warmup_end")]
+    df_scaled = df.copy()
+
+    if method == "standard":
+        scaler = preprocessing.StandardScaler()
+        df_scaled[columns] = scaler.fit_transform(df_scaled[columns])
+
+    if method == "min_max":
+        scaler = preprocessing.MinMaxScaler()
+        df_scaled[columns] = scaler.fit_transform(df_scaled[columns])
+
+    if method == "max_abs":
+        scaler = preprocessing.MaxAbsScaler()
+        df_scaled[columns] = scaler.fit_transform(df_scaled[columns])
+
+    if (
+        method == "power"
+    ):  # default is Yeo-Johnson, Box-Cox Tranformation is not applicable
+        scaler = preprocessing.PowerTransformer(method="yeo-johnson", standardize=False)
+        df_scaled[columns] = scaler.fit_transform(df_scaled[columns])
+
+    return df_scaled
+
  
 
   
