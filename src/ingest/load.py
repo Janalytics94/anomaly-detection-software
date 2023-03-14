d65ba332a10331409bdd4569a6c4d7a37591d765
Janalytics944 <jvihs@yahoo.com>
2023-03-14 18:43:16 +0100

    added data from CVE-2021-46529

--- src/ingest/load.py
+++ src/ingest/load.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-from dataloader.dataloader_factory import dataloader_factory
+import os
 import logging
 import pandas as pd
 import numpy as np
+import glob
+from dataloader.dataloader_factory import dataloader_factory
 from datetime import datetime
 from clize import run
     
 
-def load(src:str, scenario:str , type:str, target:str):
+def load(src:str, scenario:str, type:str, target:str):
 
     """
     Function to load .res files from selected scenario
@@ -19,57 +21,83 @@ def load(src:str, scenario:str , type:str, target:str):
     Returns:
     pd.Dataframe for each recording
     """
-   
-    dataloader = dataloader_factory(src+'/'+scenario)
-    
-    if type =='train':
-        RAW = {
-            scenario: dataloader.training_data()
-        }
-    if type == 'test':
-        RAW = {
-            scenario: dataloader.test_data()
-        }
-    if type == 'validation':
-        RAW = {
-            scenario: dataloader.validation_data(),
-        }
 
-    container_names = []
-    resource_stats_l = []
-    jsons = []
+    
+    if (scenario == "CVE-2021-46529") & (type == "train"):
+        list_ = []
+        files = glob.glob(os.path.join(src, scenario, "normal/*.res"))
+        for file in files:
+            data = pd.read_csv(file, delimiter=',', index_col='timestamp', parse_dates=True)
+            list_.append(data)
+        RAW = {scenario: pd.concat(list_)}
+        resources = RAW[scenario]
+        resources = resources.fillna(0)
+    
+    if (scenario == "CVE-2021-46529") & (type == "test"):
+        list_ = []
+        files = glob.glob(os.path.join(src, scenario, "attack/*.res"))
+        for file in files:
+            data = pd.read_csv(file, delimiter=',', index_col='timestamp', parse_dates=True)
+            list_.append(data)
+        RAW = {scenario: pd.concat(list_)}
+        resources = RAW[scenario]
+        resources = resources.fillna(0)
+    
+    if scenario != "CVE-2021-46529":
+        dataloader = dataloader_factory(src+'/'+scenario)
+        
+        if type =='train':
+            RAW = {
+                scenario: dataloader.training_data()
+            }
+        if type == 'test':
+            RAW = {
+                scenario: dataloader.test_data()
+            }
+        if type == 'validation':
+            RAW = {
+                scenario: dataloader.validation_data(),
+            }
 
- 
-    for i in range(0, len(RAW[scenario])):
-        container_names.append(RAW[scenario][i].name)
-        resource_stats_l.append(RAW[scenario][i].resource_stats())
-        jsons.append(RAW[scenario][i].metadata())
+        container_names = []
+        resource_stats_l = []
+        jsons = []
+
     
-    resource_stats = {}
-    for h in range(0,len(container_names)):
-        resource_stats[container_names[h]] = {
-                "timestamp": [resource_stats_l[h][q].timestamp_datetime() for q in range(0,len(resource_stats_l[h]))],
-                "cpu_usage": [resource_stats_l[h][q].cpu_usage() for q in range(0,len(resource_stats_l[h]))],
-                "memory_usage": [resource_stats_l[h][q].memory_usage() for q in range(0,len(resource_stats_l[h]))],
-                "network_received": [resource_stats_l[h][q].network_received() for q in range(0,len(resource_stats_l[h]))],
-                "network_send": [resource_stats_l[h][q].network_send() for q in range(0,len(resource_stats_l[h]))],
-                "storage_read": [resource_stats_l[h][q].storage_read() for q in range(0,len(resource_stats_l[h]))],
-                "storage_written": [resource_stats_l[h][q].storage_written() for q in range(0,len(resource_stats_l[h]))],
-                "exploit": np.repeat(jsons[h]['exploit'], len(resource_stats_l[h])),
-                "timestamp_container_ready": np.repeat(datetime.fromtimestamp(jsons[h]["time"]["container_ready"]["absolute"]), len(resource_stats_l[h])),
-                "timestamp_warmup_end": np.repeat(datetime.fromtimestamp(jsons[h]["time"]["warmup_end"]["absolute"]), len(resource_stats_l[h]))
-        }
-    resources = pd.DataFrame(resource_stats)
-    resources = resources.transpose()
-    resources = resources.explode(resources.columns.tolist())
-    resources = resources.reset_index()
-    print(resources)
-    # get container names
-    #resources["container_name"] = resources.index
-    # split timestamp 
-    resources["dates"] = resources["timestamp"].dt.date
-    resources["times"] = resources["timestamp"].dt.time
-    
+        for i in range(0, len(RAW[scenario])):
+            container_names.append(RAW[scenario][i].name)
+            resource_stats_l.append(RAW[scenario][i].resource_stats())
+            jsons.append(RAW[scenario][i].metadata())
+        
+        resource_stats = {}
+        for h in range(0,len(container_names)):
+            resource_stats[container_names[h]] = {
+                    "timestamp": [resource_stats_l[h][q].timestamp_datetime() for q in range(0,len(resource_stats_l[h]))],
+                    "cpu_usage": [resource_stats_l[h][q].cpu_usage() for q in range(0,len(resource_stats_l[h]))],
+                    "memory_usage": [resource_stats_l[h][q].memory_usage() for q in range(0,len(resource_stats_l[h]))],
+                    "network_received": [resource_stats_l[h][q].network_received() for q in range(0,len(resource_stats_l[h]))],
+                    "network_send": [resource_stats_l[h][q].network_send() for q in range(0,len(resource_stats_l[h]))],
+                    "storage_read": [resource_stats_l[h][q].storage_read() for q in range(0,len(resource_stats_l[h]))],
+                    "storage_written": [resource_stats_l[h][q].storage_written() for q in range(0,len(resource_stats_l[h]))],
+                    "exploit": np.repeat(jsons[h]['exploit'], len(resource_stats_l[h])),
+                    "timestamp_container_ready": np.repeat(datetime.fromtimestamp(jsons[h]["time"]["container_ready"]["absolute"]), len(resource_stats_l[h])),
+                    "timestamp_warmup_end": np.repeat(datetime.fromtimestamp(jsons[h]["time"]["warmup_end"]["absolute"]), len(resource_stats_l[h]))
+            }
+        resources = pd.DataFrame(resource_stats)
+        resources = resources.transpose()
+        resources = resources.explode(resources.columns.tolist())
+        resources = resources.reset_index()
+        print(resources)
+        # get container names
+        #resources["container_name"] = resources.index
+        # split timestamp 
+        resources["dates"] = resources["timestamp"].dt.date
+        resources["times"] = resources["timestamp"].dt.time
+        # encode 
+        resources["exploit"] = resources["exploit"].astype(int)
+        y_true = resources["exploit"]
+        y_true.to_csv(target + '/y_' + type + '.csv', sep=';')
+        
     # make sure eveything has the same dtype
     resources["cpu_usage"] = resources["cpu_usage"].astype(float)
     resources["memory_usage"] = resources["memory_usage"].astype(int)
@@ -81,9 +109,7 @@ def load(src:str, scenario:str , type:str, target:str):
     _logger = logging.getLogger(__name__)
     _logger.warning(f"Preprocessing data : {type}")
     
-    # encode 
-    resources["exploit"] = resources["exploit"].astype(int)
-    y_true = resources["exploit"]
+    
     resources = resources[['cpu_usage','memory_usage']]#'network_received','network_send','storage_read','storage_written']]	
     # Outlier Trunctuate 
     Q1=resources.quantile(0.25)
@@ -92,7 +118,7 @@ def load(src:str, scenario:str , type:str, target:str):
     df = resources[~((resources<(Q1-1.5*IQR)) | (resources>(Q3+1.5*IQR)))]
     _logger.warning("Saving data...")
     df.to_csv(target + '/' + type + '.csv', sep=';')
-    y_true.to_csv(target + '/y_' + type + '.csv', sep=';')
+    
 
     return 
 
